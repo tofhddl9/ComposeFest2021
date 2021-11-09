9d6819222720c1c7a8c264908abf44c3ab17679b
tofhddl9 <tofhddl9@gmail.com>
2021-11-10 02:00:41 +0900

    [Week1] Implement up to ComposeBasics-11

--- week1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MyApp() {
   
-  var shouldShowOnboarding by remember { mutableStateOf(true) }
+  var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
   
   if (shouldShowOnboarding) {
     OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
@@ -56,10 +62,10 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-  Column(modifier = Modifier.padding(vertical = 4.dp)) {
-    for (name in names) {
-      Greeting(name = name)
+private fun Greetings(names: List<String> = List(100) {"$it"}) {
+  LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
+      Greeting(name)
     }
   }
 }
@@ -67,9 +73,15 @@ private fun Greetings(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun Greeting(name: String) {
   
-  val expanded = remember { mutableStateOf(false) }
+  val expanded = rememberSaveable { mutableStateOf(false) }
   
-  val extraPadding = if (expanded.value) 48.dp else 0.dp
+  val extraPadding by animateDpAsState(
+    targetValue = if (expanded.value) 48.dp else 0.dp,
+    animationSpec = spring(
+      dampingRatio = Spring.DampingRatioMediumBouncy,
+      stiffness = Spring.StiffnessLow
+    )
+  )
   
   Surface(
     color = MaterialTheme.colors.primary,
@@ -78,7 +90,7 @@ fun Greeting(name: String) {
     Row(modifier = Modifier.padding(24.dp)) {
       Column(modifier = Modifier
         .weight(1f)
-        .padding(bottom = extraPadding)
+        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
       ) {
         Text(text = "Hello,")
         Text(text = "$name!")
