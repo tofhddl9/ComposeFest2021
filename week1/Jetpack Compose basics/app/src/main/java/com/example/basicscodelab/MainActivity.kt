e6fbf9f6df9bc15b56ff357194578905f520e419
tofhddl9 <tofhddl9@gmail.com>
2021-11-10 02:37:18 +0900

    [Week1] Modify indent

--- week1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,130 +15,127 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      BasicsCodelabTheme {
-        MyApp()
-      }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                MyApp()
+            }
+        }
     }
-  }
 }
 
 @Composable
 private fun MyApp() {
-  
-  var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-  
-  if (shouldShowOnboarding) {
-    OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-  } else {
-    Greetings()
-  }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
 }
 
 @Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
-  
-  Surface {
-    Column(
-      modifier = Modifier.fillMaxSize(),
-      verticalArrangement = Arrangement.Center,
-      horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-      Text("Welcome to the Basics Codelab!")
-      Button(
-        modifier = Modifier.padding(vertical = 24.dp),
-        onClick = { onContinueClicked() }
-      ) {
-        Text("Continue")
-      }
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onContinueClicked() }
+            ) {
+                Text("Continue")
+            }
+        }
     }
-  }
 }
 
 @Composable
 private fun Greetings(names: List<String> = List(100) {"$it"}) {
-  LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-    items(items = names) { name ->
-      Greeting(name)
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name)
+        }
     }
-  }
 }
 
 @Composable
 fun Greeting(name: String) {
-  
-  var expanded by rememberSaveable { mutableStateOf(false) }
-  
-  val extraPadding by animateDpAsState(
-    targetValue = if (expanded) 48.dp else 0.dp,
-    animationSpec = spring(
-      dampingRatio = Spring.DampingRatioMediumBouncy,
-      stiffness = Spring.StiffnessLow
-    )
-  )
-  
-  Surface(
-    color = MaterialTheme.colors.primary,
-    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
-  ) {
-    Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier
-        .weight(1f)
-        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-      ) {
-        Text(text = "Hello,")
-        Text(text = "$name!", style = MaterialTheme.typography.h4.copy(
-          fontWeight = FontWeight.ExtraBold
-        ))
-        if (expanded) {
-          Text(text = ("expand expand expand expand expand expand").repeat(4))
-        }
-      }
-      IconButton(onClick = {expanded = !expanded}) {
-        Icon(
-          imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-          contentDescription = if (expanded) {
-            "show less"
-          } else {
-            "show more"
-          }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
         )
-      }
+    )
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name!", style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
+                if (expanded) {
+                    Text(text = ("expand expand expand expand expand expand").repeat(4))
+                }
+            }
+            IconButton(onClick = {expanded = !expanded}) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        "show less"
+                    } else {
+                        "show more"
+                    }
+                )
+            }
+        }
     }
-  }
 }
 
 @Preview(
-  showBackground = true,
-  widthDp = 320,
-  uiMode = UI_MODE_NIGHT_YES,
-  name = "DefaultPreviewDark"
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
 )
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
-  BasicsCodelabTheme {
-    Greetings()
-  }
+    BasicsCodelabTheme {
+        Greetings()
+    }
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
-  BasicsCodelabTheme {
-    OnboardingScreen(onContinueClicked = {})
-  }
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
 }
