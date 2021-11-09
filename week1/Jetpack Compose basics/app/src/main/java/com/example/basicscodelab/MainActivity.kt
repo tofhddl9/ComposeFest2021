6b61dd9547416a93fbf15c00f8cf89ce731022aa
tofhddl9 <tofhddl9@gmail.com>
2021-11-10 02:17:23 +0900

    [Week1] Done ComposeBasics

--- week1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +11,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -73,10 +79,10 @@ private fun Greetings(names: List<String> = List(100) {"$it"}) {
 @Composable
 fun Greeting(name: String) {
   
-  val expanded = rememberSaveable { mutableStateOf(false) }
+  var expanded by rememberSaveable { mutableStateOf(false) }
   
   val extraPadding by animateDpAsState(
-    targetValue = if (expanded.value) 48.dp else 0.dp,
+    targetValue = if (expanded) 48.dp else 0.dp,
     animationSpec = spring(
       dampingRatio = Spring.DampingRatioMediumBouncy,
       stiffness = Spring.StiffnessLow
@@ -93,16 +99,34 @@ fun Greeting(name: String) {
         .padding(bottom = extraPadding.coerceAtLeast(0.dp))
       ) {
         Text(text = "Hello,")
-        Text(text = "$name!")
+        Text(text = "$name!", style = MaterialTheme.typography.h4.copy(
+          fontWeight = FontWeight.ExtraBold
+        ))
+        if (expanded) {
+          Text(text = ("expand expand expand expand expand expand").repeat(4))
+        }
       }
-      OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-        Text(if(expanded.value) "Show less" else "Show more")
+      IconButton(onClick = {expanded = !expanded}) {
+        Icon(
+          imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+          contentDescription = if (expanded) {
+            "show less"
+          } else {
+            "show more"
+          }
+        )
       }
     }
   }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(
+  showBackground = true,
+  widthDp = 320,
+  uiMode = UI_MODE_NIGHT_YES,
+  name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
   BasicsCodelabTheme {
